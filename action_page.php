7ca9b2532c02d01e1117069c5d3d64acaf8905da
cutiedooba <132956352+cutiedooba@users.noreply.github.com>
2024-07-24 13:52:02 -0400

    changes to home screen

---
+++ action_page.php
@@ -0,0 +1,29 @@
+<?php
+
+var_dump($_SERVER["REQUEST_METHOD"]);
+header("Location ../contact.html")
+?>
+
+/*if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstname = htmlspecialchars($_POST["firstname"])
+    $lastname = htmlspecialchars($_POST["lastname"])
+    $email = htmlspecialchars($_POST["email"])
+    $message = htmlspecialchars($_POST["message"])
+
+    echo "This is the data, the user submitted";
+    echo "<br>";
+    echo $firstname;
+    echo "<br>";
+    echo $lastname;
+    echo "<br>";
+    echo $email;
+    echo "<br>";
+    echo $message;
+
+    header("Location ../contact.html")
+    
+
+
+}
+
+
---
+++ data_page.php
@@ -0,0 +1,20 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstname = htmlspecialchars($_POST["firstname"])
+    $lastname = htmlspecialchars($_POST["lastname"])
+    $email = htmlspecialchars($_POST["email"])
+    $message = htmlspecialchars($_POST["message"])
+
+    echo "This is the data, the user submitted";
+    echo "<br>";
+    echo $firstname;
+    echo "<br>";
+    echo $lastname;
+    echo "<br>";
+    echo $email;
+    echo "<br>";
+    echo $message;
+
+    header("Location ../contact.html")
+}
